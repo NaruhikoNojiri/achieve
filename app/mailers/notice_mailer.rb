b558ec7d9c547a276e132d3163f987f1f6f37b37
野尻稔彦 <nojiritoshihiko@nojiritoshihiko-no-MacBook-Air.local>
2017-07-22 22:03:48 +0900

    commit for task11 secondly

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -14,7 +14,7 @@ class NoticeMailer < ApplicationMailer
   def sendmail_contact(contact)
     @contact = contact
 
-    mail to: "n.nojiri0620@gmail.com",
+    mail to: @contact.email,
       subject: "【Achieve】お問い合わせが完了しました"
   end
 
