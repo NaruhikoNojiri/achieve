e5dac6e699184dc19ebc013e783e35f7a43344a7
野尻稔彦 <nojiritoshihiko@nojiritoshihiko-no-MacBook-Air.local>
2018-05-28 21:30:53 +0900

    rspec test for blog model was implemented.

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -2,9 +2,22 @@ require ('rails_helper')
 
 describe Blog do
 
-  # it is valid when it has title.
-  it "is valid wiht title"
+  # it is valid when it has title and content.
+  it "is valid with title and content" do
+    blog = Blog.new(title:'野尻', content:'暑いです')
+    expect(blog).to(be_valid)
+  end
 
   # it is invalid when it has no title.
-  it "is invalid without a title"
-end
+  it "is invalid without a title" do
+    blog = Blog.new
+    expect(blog).not_to(be_valid)
+  end
+
+  # it is valid when it has title and content.
+  it "is valid with title" do
+    blog = Blog.new
+    blog.valid?
+    expect(blog.errors[:title]).to(include("を入力してください"))
+  end
+ end
